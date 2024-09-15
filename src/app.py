b75bfee8f3991eff847d3baa8fcb0614faa227b7
geekzyn <abdelali.ezzyn@gmail.com>
2024-09-15 23:28:33 +0200

    add minimal calculate func

--- src/app.py
+++ src/app.py
@@ -7,5 +7,16 @@ app = Flask(__name__)
 def index():
     return "<p>Hello, World!<p>"
 
+@app.route('/calculate', methods=['POST'])
+def calculate():
+    req_data = request.get_json()
+    daily_rate = req_data['daily_rate']
+    days_worked = req_data['days_worked']
+    return jsonify({
+        'daily_rate': daily_rate,
+        'days_worked': days_worked,
+        'revenue': daily_rate * days_worked
+    })
+
 if __name__ == '__main__':
     app.run(debug=True)
